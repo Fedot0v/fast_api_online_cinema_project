9e2cd168db3a3ea477198686f725c70c025511de
nikit <nikita.fedotov222@gmail.com>
2025-04-11 14:17:01 +0300

    add user pofile model and token models

--- database/models/accounts.py
+++ database/models/accounts.py
@@ -1,5 +1,5 @@
 import enum
-from datetime import datetime
+from datetime import datetime, date, timezone, timedelta
 from typing import List, Optional
 
 from sqlalchemy import (
@@ -9,13 +9,14 @@ from sqlalchemy import (
     Boolean,
     DateTime,
     func,
-    ForeignKey
+    ForeignKey, Date, Text, UniqueConstraint
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from database.validators import accounts as validators
 from security.passwords import hash_password
+from security.utils import generate_secure_token
 
 Base = declarative_base()
 
@@ -161,3 +162,131 @@ class UserModel(Base):
     @validates("email")
     def validate_email(self, key, value):
         return validators.validate_email(value.lower())
+
+
+class UserProfileModel(Base):
+    __tablename__ = "user_profiles"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
+    avatar: Mapped[str] = mapped_column(String(255))
+    gender: Mapped[UserGenderEnum] = mapped_column(
+        Enum(UserGenderEnum)
+    )
+    date_of_birth: Mapped[Optional[date]] = mapped_column(Date)
+    info: Mapped[Optional[str]] = mapped_column(Text)
+    user_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True
+    )
+
+    user: Mapped["UserModel"] = relationship(
+        "UserModel",
+        back_populates="profile"
+    )
+
+    def __repr__(self):
+        return (
+            f"<UserProfileModel(id={self.id}, first_name={self.first_name},"
+            f" last_name={self.last_name}, "
+            f"gender={self.gender}, date_of_birth={self.date_of_birth})>"
+        )
+
+
+class TokenBaseModel(Base):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    token: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        unique=True,
+        default=generate_secure_token,
+    )
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc) + timedelta(days=1)
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+
+class ActivationTokenModel(TokenBaseModel):
+    __tablename__ = "activation_tokens"
+
+    user: Mapped[UserModel] = relationship(
+        "UserModel",
+        back_populates="activation_token"
+    )
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+    def __repr__(self):
+        return (f"<ActivationTokenModel(id={self.id}, token={self.token},"
+                f" expires_at={self.expires_at})>")
+
+
+class PasswordResetTokenModel(TokenBaseModel):
+    __tablename__ = "password_reset_tokens"
+
+    user: Mapped[UserModel] = relationship(
+        "UserModel",
+        back_populates="password_reset_token"
+    )
+
+    __table_args__ = (UniqueConstraint("user_id"),)
+
+    def __repr__(self):
+        return (f"<PasswordResetTokenModel(id={self.id}, token={self.token},"
+                f" expires_at={self.expires_at})>")
+
+
+class RefreshTokenModel(TokenBaseModel):
+    __tablename__ = "refresh_tokens"
+
+    user: Mapped[UserModel] = relationship(
+        "UserModel",
+        back_populates="refresh_tokens"
+    )
+    token: Mapped[str] = mapped_column(
+        String(512),
+        unique=True,
+        nullable=False,
+        default=generate_secure_token
+    )
+
+    @classmethod
+    def create(
+            cls,
+            user_id: int | Mapped[int],
+            days_valid: int,
+            token: str
+    ) -> "RefreshTokenModel":
+        """
+        Factory method to create a new RefreshTokenModel instance.
+
+        This method simplifies the creation of a new refresh token by calculating
+        the expiration date based on the provided number of valid days and setting
+        the required attributes.
+        """
+        expires_at = datetime.now(timezone.utc) + timedelta(days=days_valid)
+        return cls(user_id=user_id, expires_at=expires_at, token=token)
+
+    def __repr__(self):
+        return (f"<RefreshTokenModel(id={self.id}, token={self.token},"
+                f" expires_at={self.expires_at})>")
