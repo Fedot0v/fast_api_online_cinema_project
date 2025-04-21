a557598702b5f616a438ec4dd584290689af678f
nikit <nikita.fedotov222@gmail.com>
2025-04-21 14:19:49 +0300

    "Refactor settings: add new email-related configurations, improve environment handling, and define `.env` file support using Pydantic's `SettingsConfigDict`."

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,34 +1,53 @@
 import os
 from pathlib import Path
-
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "movies.db")
     PATH_TO_MOVIES_CSV: str = str(BASE_DIR / "database" / "seed_data" / "imdb_movies.csv")
-    SECRET_KEY_ACCESS: str = "SECRET_KEY_ACCESS"
-    SECRET_KEY_REFRESH: str = "SECRET_KEY_REFRESH"
+    SECRET_KEY_ACCESS: str
+    SECRET_KEY_REFRESH: str
     JWT_SIGNING_ALGORITHM: str = "HS256"
+    EMAIL_HOSTNAME: str
+    EMAIL_PORT: int = 587
+    EMAIL_ADDRESS: str
+    EMAIL_PASSWORD: str
+    EMAIL_USE_TLS: bool = False
+    EMAIL_TEMPLATE_DIR: str = "templates/email"
+    ACTIVATION_EMAIL_TEMPLATE: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE: str = "activation_complete.html"
+    PASSWORD_EMAIL_TEMPLATE: str = "password_reset_request.html"
+    PASSWORD_COMPLETE_EMAIL_TEMPLATE: str = "password_reset_complete.html"
+    APP_BASE_URL: str = "http://localhost:8000"
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
 
 class TestingSettings(Settings):
     PATH_TO_DB: str = ":memory:"
-
+    EMAIL_HOSTNAME: str = "localhost"
+    EMAIL_PORT: int = 1025
+    EMAIL_ADDRESS: str = "test@example.com"
+    EMAIL_PASSWORD: str = ""
+    EMAIL_USE_TLS: bool = False
+    SECRET_KEY_ACCESS: str = "test_access_key"  # Значение для тестов
+    SECRET_KEY_REFRESH: str = "test_refresh_key"  # Значение для тестов
 
 def get_settings() -> BaseSettings:
     """
     Retrieve the application settings based on the environment.
 
-    This function checks the `ENVIRONMENT` environment variable to determine
-    which settings class to use. If `ENVIRONMENT` is set to `"testing"`, it
-    returns an instance of `TestingSettings`. Otherwise, it defaults to `Settings`.
-
     :return: An instance of the appropriate settings class.
     :rtype: BaseSettings
     """
-    environment = os.getenv("ENVIRONMENT", "testing")
+    environment = os.getenv("ENVIRONMENT", "development")  # Изменено на "development"
     if environment == "testing":
         return TestingSettings()
     return Settings()
