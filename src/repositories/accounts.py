c3c5c8efcc1c5b48c0dbab155255f03353c38fe9
nikit <nikita.fedotov222@gmail.com>
2025-04-22 15:41:57 +0300

    "Refactored account repositories: removed `ProfileRepository` and redundant imports, integrated base repository inheritance to streamline code."

--- src/repositories/accounts.py
+++ src/repositories/accounts.py
@@ -1,21 +1,13 @@
-from datetime import date, datetime, timezone
-from typing import Type, Sequence, List
+from datetime import date
+from typing import Type, List
 
 from fastapi import HTTPException, status
 from sqlalchemy import select, delete, update
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from src.database.models.accounts import UserModel, GenderEnum, UserProfileModel, RefreshTokenModel, ActivationTokenModel, \
     PasswordResetTokenModel, TokenBaseModel
-
-
-class BaseAccountRepository:
-    def __init__(
-            self,
-            db: AsyncSession,
-    ):
-        self.db = db
+from src.repositories.base import BaseAccountRepository
 
 
 class UserRepository(BaseAccountRepository):
@@ -61,77 +53,6 @@ class UserRepository(BaseAccountRepository):
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An error occurred during password update."
             )
-
-
-class ProfileRepository(BaseAccountRepository):
-    async def create_profile(
-            self,
-            user_id: int,
-            first_name: str,
-            last_name: str,
-            avatar: str,
-            gender: GenderEnum,
-            date_of_birth: date | None = None,
-            info: str | None = None
-    ) -> UserProfileModel:
-        profile = UserProfileModel(
-            user_id=user_id,
-            first_name=first_name,
-            last_name=last_name,
-            avatar=avatar,
-            gender=gender,
-            date_of_birth=date_of_birth,
-            info=info
-        )
-        self.db.add(profile)
-        return profile
-
-    async def update_profile(
-            self,
-            user_id: int,
-            first_name: str | None = None,
-            last_name: str | None = None,
-            avatar: str | None = None,
-            gender: GenderEnum | None = None,
-            date_of_birth: date | None = None,
-            info: str | None = None
-    ) -> UserProfileModel:
-        stmt = select(UserProfileModel).where(
-            UserProfileModel.user_id == user_id
-        )
-        result = await self.db.execute(stmt)
-        profile = result.scalars().first()
-
-        if profile:
-            updates = {}
-            if first_name is not None:
-                updates["first_name"] = first_name
-            if last_name is not None:
-                updates["last_name"] = last_name
-            if avatar is not None:
-                updates["avatar"] = avatar
-            if gender is not None:
-                updates["gender"] = gender
-            if date_of_birth is not None:
-                updates["date_of_birth"] = date_of_birth
-            if info is not None:
-                updates["info"] = info
-            if updates:
-                stmt = update(UserProfileModel).where(
-                    UserProfileModel.user_id == user_id
-                ).values(**updates)
-                await self.db.execute(stmt)
-
-            await self.db.refresh(profile)
-        return profile
-
-    async def get_profile_by_user_id(
-            self,
-            user_id: int
-    ) -> UserProfileModel | None:
-        stmt = select(UserProfileModel).where(UserProfileModel.user_id == user_id)
-        result = await self.db.execute(stmt)
-        return result.scalars().first()
 
 
 class BaseTokenRepository(BaseAccountRepository):
